5fe5d609d67d8857299fb8c6b96e0e08909e059a
Vicky L <vicky.sy.loo@gmail.com>
2016-04-27 00:02:45 -0400

    editing game

---
+++ ttt.js
@@ -0,0 +1,33 @@
+$(document).on('ready', function () {
+  var winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+  var turn = 0;
+
+  function checkWinner() {
+    for(var i = 0; i < winningCombos.length; i++) {
+      var combo = winningCombos[i];
+      
+      if (($('.x').hasClass(combo[0]) && $('.x').hasClass(combo[1]) && $('.x').hasClass(combo[2])) {
+      // combo will be [1,2,3]
+      // compare each combo to a playerState array or use the DOM => $('.x')
+      alert("We have a winner!");
+    };
+  };
+
+
+
+  $('td').on('click', function () {
+
+    if (turn % 2 === 0) {
+      $(this).html("X").addClass("x");
+    } else {
+      $(this).html("O").addClass("o");
+    }
+
+    checkWinner();
+
+    $(this).off('click');
+    turn++;
+
+  });
+
+});
