fa1936f95a2bca9a0d41683611104e2b0ffd2fa3
Vicky L <vicky.sy.loo@gmail.com>
2016-05-28 16:40:53 -0400

    fixing javascript, basic game function working

--- ttt.js
+++ ttt.js
@@ -1,19 +1,17 @@
-$(document).on('ready', function () {
+$(document).on('ready page:load', function () {
   var winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
   var turn = 0;
 
   function checkWinner() {
-    for(var i = 0; i < winningCombos.length; i++) {
-      var combo = winningCombos[i];
-
-      if (($('.x').hasClass(combo[0]) || $('.x').hasClass(combo[1]) || $('.x').hasClass(combo[2])) 
-      // combo will be [1,2,3]
-      // compare each combo to a playerState array or use the DOM => $('.x')
-      alert("We have a winner!");
+    for(var num in winningCombos){
+        if($('.x').hasClass(winningCombos[num][0]) && $('.x').hasClass(winningCombos[num][1]) && $('.x').hasClass(winningCombos[num][2])){
+          alert('X wins');
+        }
+        else if($('.o').hasClass(winningCombos[num][0]) && $('.o').hasClass(winningCombos[num][1]) && $('.o').hasClass(winningCombos[num][2])){
+          alert('O wins')
+        }
     };
   };
-
-
 
   $('td').on('click', function () {
 
