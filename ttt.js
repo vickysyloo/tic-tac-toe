a94c9643b7bab255b0b859fe63803388d74009f0
Vicky L <vicky.sy.loo@gmail.com>
2016-05-28 17:07:22 -0400

    adding game tied, still fixing game restart mode

--- ttt.js
+++ ttt.js
@@ -2,30 +2,59 @@ $(document).on('ready page:load', function () {
   var winningCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
   var turn = 0;
 
-  function checkWinner() {
-    for(var num in winningCombos){
-        if($('.x').hasClass(winningCombos[num][0]) && $('.x').hasClass(winningCombos[num][1]) && $('.x').hasClass(winningCombos[num][2])){
-          alert('X wins');
-        }
-        else if($('.o').hasClass(winningCombos[num][0]) && $('.o').hasClass(winningCombos[num][1]) && $('.o').hasClass(winningCombos[num][2])){
-          alert('O wins')
-        }
-    };
-  };
-
   $('td').on('click', function () {
 
     if (turn % 2 === 0) {
       $(this).html("X").addClass("x");
     } else {
       $(this).html("O").addClass("o");
-    }
+    };
 
-    checkWinner();
+    checkWinner ();
 
     $(this).off('click');
     turn++;
-
   });
+
+  function checkWinner () {
+    for(var num in winningCombos){
+        if($('.x').hasClass(winningCombos[num][0]) && $('.x').hasClass(winningCombos[num][1]) && $('.x').hasClass(winningCombos[num][2])){
+          alert('X wins');
+          resetGame();
+        }
+        else if($('.o').hasClass(winningCombos[num][0]) && $('.o').hasClass(winningCombos[num][1]) && $('.o').hasClass(winningCombos[num][2])){
+          alert('O wins')
+          resetGame();
+        }
+    };
+  };
+
+  function gameTied () {
+      if(turn === 9 && checkWinner() === false){
+        alert('No winners! Game tied.');
+        resetGame();
+      }
+  };
+
+  // function newGame () {
+  //   $('td').on('click', function () {
+  //     if (turn % 2 === 0) {
+  //       $(this).html("X").addClass("x");
+  //     } else {
+  //       $(this).html("O").addClass("o");
+  //     };
+  //
+  //     checkWinner ();
+  //
+  //     $(this).off('click');
+  //     turn++;
+  //   });
+  // };
+
+  function resetGame () {
+      $('td').removeClass("x").html("");
+      $('td').removeClass("o").html("");
+      // newGame ();
+  };
 
 });
